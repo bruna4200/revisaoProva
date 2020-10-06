58114b3106f2a361db1dc4e35fc106aadb96ce23
Bruna Rodrigues <brunaradin@hotmail.com>
2020-10-06 20:37:57 -0300

    primeira versao

---
+++ ProvaH1/Controllers/TimeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProvaH1.Models;
+
+namespace ProvaH1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Listagem de times");
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok("Listagem de um time");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(Time time)
+        {
+            return Ok("Time cadastrado!!!");
+        }
+    }
+}
